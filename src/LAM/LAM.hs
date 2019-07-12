1acdb6e2155a036941c2cdf97f2f121ce1e61419
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-07-12 17:58:54 +0200

    :recycle: Refactored imports for better readability

--- src/LAM/LAM.hs
+++ src/LAM/LAM.hs
@@ -2,6 +2,7 @@
 
 module LAM.LAM where
 
+import LL.Types (Combinator((:<>)), Combinator((:<)), Combinator((:⌷)), Combinator((:⎨)), Combinator((:∘)), Combinator((:×)), Value((:⌟)), Value((:·)))
 import qualified LL.Types as LL
 import LAM.Types
 import Prelude (($), (.), error)
@@ -9,9 +10,9 @@ import Debug.Trace (traceShow)
 
 linear :: forall a b d. LL.Combinator a b → Code Instruction a b d d
 linear LL.Id = Empty
-linear (y LL.:∘ x) = linear x +. linear y
+linear (y :∘ x) = linear x +. linear y
 linear LL.Eone = Empty
-linear (x LL.:× y) = Pushl `Cons` (linear x +. (Consl `Cons` (Pushr `Cons` (linear y +. list Consr))))
+linear (x :× y) = Pushl `Cons` (linear x +. (Consl `Cons` (Pushr `Cons` (linear y +. list Consr))))
 linear LL.Ol = list Ol
 linear LL.Cl = list Cl
 linear LL.Or = list Or
@@ -19,14 +20,14 @@ linear LL.Cr = list Cr
 linear LL.Ex = list Ex
 linear LL.Al = list Al
 linear LL.Ar = list Ar
-linear (LL.:<>) = list $ Constr (LL.:<>)
-linear (x LL.:< y) = list $ Constr (x LL.:< y)
+linear (:<>) = list $ Constr (:<>)
+linear (x :< y) = list $ Constr (x :< y)
 linear LL.Fst = list Fst
 linear LL.Snd = list Snd
-linear (LL.:⌷) = list Ø
+linear (:⌷) = list Ø
 linear LL.Inl = list $ Constr LL.Inl
 linear LL.Inr = list $ Constr LL.Inr
-linear (x LL.:⎨ y) = list $ Altv (linear x) (linear y)
+linear (x :⎨ y) = list $ Altv (linear x) (linear y)
 linear (LL.Lcur x) = list $ Constr (LL.Lcur x)
 linear LL.Lapp = list Lapp
 linear (LL.Make x y z) =  list $ Constr (LL.Make x y z)
@@ -38,28 +39,28 @@ linear LL.Succ = list $ Constr LL.Succ
 linear (LL.Nrec x y) = list $ Nrec (linear x) (linear y)
 
 run :: forall a b d e. Code Instruction a b d e → LL.Value a → Dump d → LL.Value b
-run (Pushl `Cons` c) (u LL.:⌟ v) d = run c u (v `Push` d)
-run (Consl `Cons` c) u (v `Push` d) = run c (u LL.:⌟ v) d
-run (Pushr `Cons` c) (u LL.:⌟ v) d = run c v (u `Push` d)
-run (Consr `Cons` c) v (u `Push` d) = run c (u LL.:⌟ v) d
-run (Ol `Cons` c) u d = run c (LL.Unit LL.:⌟ u) d
-run (Cl `Cons` c) (LL.Unit LL.:⌟ u) d = run c u d
-run (Or `Cons` c) u d = run c (u LL.:⌟ LL.Unit) d
-run (Cr `Cons` c) (u LL.:⌟ LL.Unit) d = run c u d
-run (Ex `Cons` c) (u LL.:⌟ v) d = run c (v LL.:⌟ u) d
-run (Al `Cons` c) (u LL.:⌟ (v LL.:⌟ w)) d = run c ((u LL.:⌟ v) LL.:⌟ w) d
-run (Ar `Cons` c) ((u LL.:⌟ v) LL.:⌟ w) d = run c (u LL.:⌟ (v LL.:⌟ w)) d
-run (Constr γ `Cons` c) u d = run c (γ LL.:· u) d
-run (Fst `Cons` c) ((c' LL.:< c'') LL.:· u) d = run (linear c'  +. c) u d
-run (Snd `Cons` c) ((c' LL.:< c'') LL.:· u) d = run (linear c'' +. c) u d
-run (Altv c' c'' `Cons` c) (LL.Inl LL.:· u) d = run (c'  +. c) u d
-run (Altv c' c'' `Cons` c) (LL.Inr LL.:· u) d = run (c'' +. c) u d
-run (Lapp `Cons` c) ((LL.Lcur c' LL.:· u) LL.:⌟ v) d = run (linear c' +. c) (u LL.:⌟ v) d
-run (Read `Cons` c) (LL.Make x y z LL.:· u) d = run (linear x +. c) u d
-run (Kill `Cons` c) (LL.Make x y z LL.:· u) d = run (linear y +. c) u d
-run (Dupl `Cons` c) (LL.Make x y z LL.:· u) d = run (linear z +. linear (LL.Make x y z LL.:× LL.Make x y z) +. c) u d
-run (Nrec x y `Cons` c) (LL.Zero' LL.:· y0) d = run (x +. c) LL.Unit d
-run (Nrec x y `Cons` c) (LL.Succ LL.:· n) d = run ((Nrec x y `Cons` y) +. c) n d
+run (Pushl `Cons` c) (u :⌟ v) d = run c u (v `Push` d)
+run (Consl `Cons` c) u (v `Push` d) = run c (u :⌟ v) d
+run (Pushr `Cons` c) (u :⌟ v) d = run c v (u `Push` d)
+run (Consr `Cons` c) v (u `Push` d) = run c (u :⌟ v) d
+run (Ol `Cons` c) u d = run c (LL.Unit :⌟ u) d
+run (Cl `Cons` c) (LL.Unit :⌟ u) d = run c u d
+run (Or `Cons` c) u d = run c (u :⌟ LL.Unit) d
+run (Cr `Cons` c) (u :⌟ LL.Unit) d = run c u d
+run (Ex `Cons` c) (u :⌟ v) d = run c (v :⌟ u) d
+run (Al `Cons` c) (u :⌟ (v :⌟ w)) d = run c ((u :⌟ v) :⌟ w) d
+run (Ar `Cons` c) ((u :⌟ v) :⌟ w) d = run c (u :⌟ (v :⌟ w)) d
+run (Constr γ `Cons` c) u d = run c (γ :· u) d
+run (Fst `Cons` c) ((c' :< c'') :· u) d = run (linear c'  +. c) u d
+run (Snd `Cons` c) ((c' :< c'') :· u) d = run (linear c'' +. c) u d
+run (Altv c' c'' `Cons` c) (LL.Inl :· u) d = run (c'  +. c) u d
+run (Altv c' c'' `Cons` c) (LL.Inr :· u) d = run (c'' +. c) u d
+run (Lapp `Cons` c) ((LL.Lcur c' :· u) :⌟ v) d = run (linear c' +. c) (u :⌟ v) d
+run (Read `Cons` c) (LL.Make x y z :· u) d = run (linear x +. c) u d
+run (Kill `Cons` c) (LL.Make x y z :· u) d = run (linear y +. c) u d
+run (Dupl `Cons` c) (LL.Make x y z :· u) d = run (linear z +. linear (LL.Make x y z :× LL.Make x y z) +. c) u d
+run (Nrec x y `Cons` c) (LL.Zero' :· y0) d = run (x +. c) LL.Unit d
+run (Nrec x y `Cons` c) (LL.Succ :· n) d = run ((Nrec x y `Cons` y) +. c) n d
 run Empty u Null = u
 run Empty u (d' `Push` d) = u -- Should we never happening as the dump should be empty
 run c v d = traceShow c . traceShow v . traceShow d $ error "Unreachable pattern reached!"
--- src/LAM/Types.hs
+++ src/LAM/Types.hs
@@ -2,14 +2,15 @@
 
 module LAM.Types where
 
+import LL.Types (Term((:⊗)), Term((:&)), Term((:⊸)), Term((:!)), Term(), Value())
 import qualified LL.Types as LL
 import Prelude(Show(..), String, (<>))
 
-data Dump (d :: [LL.Term]) :: * where
+data Dump (d :: [Term]) :: * where
     Null ::                          Dump '[]
-    Push :: forall d (a :: LL.Term). LL.Value a → Dump d → Dump (a : d)
+    Push :: forall d (a :: Term). Value a → Dump d → Dump (a : d)
 
-data Code (store :: LL.Term → LL.Term → [LL.Term] → [LL.Term] → *) :: LL.Term → LL.Term → [LL.Term] → [LL.Term] → * where
+data Code (store :: Term → Term → [Term] → [Term] → *) :: Term → Term → [Term] → [Term] → * where
     Empty :: forall store a d.         Code store a a d d
     Cons  :: forall store a b c d e f. store a b d e → Code store b c e f → Code store a c d f
 
@@ -23,27 +24,27 @@ list :: forall store a b d e. store a b d e → Code store a b d e
 list a = a `Cons` Empty
 
 
-data Instruction :: LL.Term → LL.Term → [LL.Term] → [LL.Term] → * where
-    Pushl  :: forall d u v.   Instruction (u 'LL.:⊗ v) u d (v : d)
-    Consl  :: forall d u v.   Instruction u (u 'LL.:⊗ v) (v : d) d
-    Pushr  :: forall d u v.   Instruction (u 'LL.:⊗ v) v d (u : d)
-    Consr  :: forall d u v.   Instruction v (u 'LL.:⊗ v) (u : d) d
-    Ol     :: forall d u.     Instruction u ('LL.One 'LL.:⊗ u) d d
-    Cl     :: forall d u.     Instruction ('LL.One 'LL.:⊗ u) u d d
-    Or     :: forall d u.     Instruction u (u 'LL.:⊗ 'LL.One) d d
-    Cr     :: forall d u.     Instruction (u 'LL.:⊗ 'LL.One) u d d
-    Ex     :: forall d u v.   Instruction (u 'LL.:⊗ v) (v 'LL.:⊗ u) d d
-    Al     :: forall d a b c. Instruction (a 'LL.:⊗ (b 'LL.:⊗ c)) ((a 'LL.:⊗ b) 'LL.:⊗ c) d d
-    Ar     :: forall d a b c. Instruction ((a 'LL.:⊗ b) 'LL.:⊗ c) (a 'LL.:⊗ (b 'LL.:⊗ c)) d d
+data Instruction :: Term → Term → [Term] → [Term] → * where
+    Pushl  :: forall d u v.   Instruction (u ':⊗ v) u d (v : d)
+    Consl  :: forall d u v.   Instruction u (u ':⊗ v) (v : d) d
+    Pushr  :: forall d u v.   Instruction (u ':⊗ v) v d (u : d)
+    Consr  :: forall d u v.   Instruction v (u ':⊗ v) (u : d) d
+    Ol     :: forall d u.     Instruction u ('LL.One ':⊗ u) d d
+    Cl     :: forall d u.     Instruction ('LL.One ':⊗ u) u d d
+    Or     :: forall d u.     Instruction u (u ':⊗ 'LL.One) d d
+    Cr     :: forall d u.     Instruction (u ':⊗ 'LL.One) u d d
+    Ex     :: forall d u v.   Instruction (u ':⊗ v) (v ':⊗ u) d d
+    Al     :: forall d a b c. Instruction (a ':⊗ (b ':⊗ c)) ((a ':⊗ b) ':⊗ c) d d
+    Ar     :: forall d a b c. Instruction ((a ':⊗ b) ':⊗ c) (a ':⊗ (b ':⊗ c)) d d
     Constr :: forall a b d.   (a `LL.Combinator` b) → Instruction a b d d
     Fst    :: forall d u v.   Instruction (u 'LL.:& v) u d d
     Snd    :: forall d u v.   Instruction (u 'LL.:& v) v d d
     Altv   :: forall d a b x. Code Instruction a x d d → Code Instruction b x d d → Instruction (a 'LL.:⊕ b) x d d
-    Lapp   :: forall d a b.   Instruction ((a 'LL.:⊸ b) 'LL.:⊗ a) b d d
+    Lapp   :: forall d a b.   Instruction ((a ':⊸ b) ':⊗ a) b d d
     Ø      :: forall d x.     Instruction 'LL.Zero x d d
-    Read   :: forall d x.     Instruction ('(LL.:!) x) x d d
-    Kill   :: forall d x.     Instruction ('(LL.:!) x) 'LL.One d d
-    Dupl   :: forall d x.     Instruction ('(LL.:!) x) ('(LL.:!) x 'LL.:⊗ '(LL.:!) x) d d
+    Read   :: forall d x.     Instruction ('(:!) x) x d d
+    Kill   :: forall d x.     Instruction ('(:!) x) 'LL.One d d
+    Dupl   :: forall d x.     Instruction ('(:!) x) ('(:!) x ':⊗ '(:!) x) d d
     Nrec   :: forall d x.     Code Instruction 'LL.One x d d → Code Instruction x x d d → Instruction 'LL.Nat x d d
 
 --------------------------------------------------------------------------------------------------------------------------
@@ -73,7 +74,7 @@ instance forall a b c d. Show (Instruction a b c d) where
     show Cr = "Instr (u ⊗ " <> show LL.One <> ") u d d"
     show Ex = "Instr (u ⊗ v) (v ⊗ u) d d"
     show Al = "Instr (a ⊗ (b ⊗ c)) ((a ⊗ b) ⊗ c) d d"
-    show Ar = "Instr ((a ⊗ b) 'LL.:⊗ c) (a 'LL.:⊗ (b 'LL.:⊗ c)) d d"
+    show Ar = "Instr ((a ⊗ b) ':⊗ c) (a ':⊗ (b ':⊗ c)) d d"
     show (Constr c1) = "(Constr " <> show c1 <> ")"
     show Fst = "Instr (u & v) u d d"
     show Snd = "Instr (u & v) v d d"
